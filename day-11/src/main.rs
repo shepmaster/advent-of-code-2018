db768d2c1ee1aa42d0cf017be3d4ecf556acbbaa
Jake Goulding <jake.goulding@gmail.com>
2018-12-12 21:40:30 -0500

    Day 11 - Part 2

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,37 +1,108 @@
 use itertools::Itertools;
-use std::collections::BTreeMap;
+use std::{iter, collections::BTreeMap, mem};
 
 const SERIAL_NUMBER: i32 = 5153;
 
 fn main() {
-    let max_top_left = max_top_left(SERIAL_NUMBER);
+    let max_top_left = max_max_top_left(SERIAL_NUMBER);
     println!("Top-left: {:?}", max_top_left);
 }
 
-fn max_top_left(serial_number: i32) -> Option<(i32, i32)> {
+type Coord = (i32, i32);
+type Grid = BTreeMap<Coord, i32>;
+
+fn grid(serial_number: i32) -> Grid {
     let coords = (1..=300).cartesian_product(1..=300);
-    let grid: BTreeMap<_, _> = coords.map(|coord| {
+    coords.map(|coord| {
         let power = power_level(coord.0, coord.1, serial_number);
         (coord, power)
-    }).collect();
+    }).collect()
+}
 
-    let coords = (1..=298).cartesian_product(1..=298);
-    coords.max_by_key(|&(x, y)| {
-        let three_by_three = (x..=x+2).cartesian_product(y..=y+2);
-        three_by_three
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct MetaSquare {
+    coord: Coord,
+    power: i32,
+    width: i32,
+}
+
+impl MetaSquare {
+    fn frontier(&self) -> impl Iterator<Item = Coord> {
+        let Self { coord: (x, y), width, .. } = *self;
+
+        let rgt = (x..x + width).map(move |x| (x, y + width));
+        let bot = (y..y + width).map(move |y| (x + width, y));
+        let cor = iter::once((x + width, y + width));
+
+        rgt.chain(bot).chain(cor)
+    }
+
+    fn grow(&self, diff: i32) -> Self {
+        let Self { coord, width, power } = *self;
+        Self { coord, width: width + 1, power: power + diff }
+    }
+}
+
+fn max_max_top_left(serial_number: i32) -> Option<MetaSquare> {
+    let grid = grid(serial_number);
+
+    let mut all_steps = Vec::new();
+
+    let mut current_step: Vec<_> = grid
+        .iter()
+        .map(|(&coord, &power)| MetaSquare { coord, power, width: 1 })
+        .collect();
+
+    for width in 0..300 {
+        eprintln!("{}", width);
+
+        let next_step = current_step.iter().flat_map(|meta_square| {
+            meta_square
+                .frontier()
+                .map(|coord| grid.get(&coord).cloned())
+                .try_fold(0, |acc, v| v.map(|v| v + acc))
+                .map(|diff| meta_square.grow(diff))
+        }).collect();
+
+        all_steps.extend(mem::replace(&mut current_step, next_step));
+    }
+
+    all_steps.into_iter().max_by_key(|ms| ms.power)
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Square {
+    coord: Coord,
+    power: i32,
+}
+
+#[cfg(test)]
+fn max_top_left(serial_number: i32, width: i32) -> Option<Square> {
+    let grid = grid(serial_number);
+    max_top_left_inner(&grid, width)
+}
+
+#[cfg(test)]
+fn max_top_left_inner(grid: &Grid, width: i32) -> Option<Square> {
+    let coords = (1..300 - width).cartesian_product(1..300 - width);
+    coords.map(|(x, y)| {
+        let three_by_three = (x..x + width).cartesian_product(y..y + width);
+        let power = three_by_three
             .flat_map(|coord| grid.get(&coord))
-            .sum::<i32>()
+            .sum::<i32>();
+        Square { coord: (x, y), power }
     })
+        .max_by_key(|square| square.power)
 }
 
 #[test]
 fn max_top_left_0() {
-    assert_eq!(max_top_left(18), Some((33, 45)));
+    assert_eq!(max_top_left(18, 3), Some(Square { coord: (33, 45), power: 29 }));
 }
 
 #[test]
 fn max_top_left_1() {
-    assert_eq!(max_top_left(42), Some((21, 61)));
+    assert_eq!(max_top_left(42, 3), Some(Square { coord: (21, 61), power: 30 }));
 }
 
 fn power_level(x: i32, y: i32, serial_number: i32) -> i32 {
